391a20467ca210dfc365c5614191dd1afc65a868
Joseph <joffan98@gmail.com>
2022-09-27 10:22:38 -0500

    commit tree af2ec1fdb037681cec0dc75ee26adf0a2fa29ac9

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const arrayCopy = [...array]
+  while (arrayCopy.length) {
+    let currentTarget = target
+    currentTarget -= arrayCopy.splice(-1)[0]
+    if (arrayCopy.find(element => element === currentTarget)) {
+      return true
+    }
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n**2)
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array, subtract the target with a number in the array
+  pop that item,
+  check if any of the remaining items in the array are equal to the new target
+  if there is a match return true
+  else continue (reset target)
+  do until no items in the array
+  then return false
 */
 
 /*
-  Add written explanation of your solution here
-*/
+  example =>
+  [5,2,-2,6,8], 10
+  start a while loop which condition is the length of the array (it will continue aslong as there is any numbers in the array)
+  start with an element (lets start with 5)
+  subtract 5 from the target 10 - 5 = 5 (new target is now 5)
+  remove 5 from the array
+  now check if 5 is in the new array 5 => [2,-2,6,8]
+  if it finds it it will return true, otherwise it will continue the while loop
+
+  outside the while loop we return false because at this point it has gone through every pair, 
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
